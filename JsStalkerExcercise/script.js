ccd6cd4cda50714b4f188f2fcbab2c109449a28c
anilsansak <anilsansak@gmail.com>
2017-09-29 17:15:55 +0300

    JS Stalker Excercise

---
+++ JsStalkerExcercise/script.js
@@ -0,0 +1,8 @@
+var firstName;
+var lastName;
+var age;
+firstName = prompt("What is your first name?");
+lastName = prompt("What is your last name?");
+age = prompt("What is your age?");
+console.log("Your full name is "+firstName+" "+lastName);
+console.log("You are "+age+" years old");
